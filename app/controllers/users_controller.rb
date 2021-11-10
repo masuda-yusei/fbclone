05e5b0aaa309cb4b6f5c5fef23bb5cd63a08199b
yusei masuda <masuda.no@icloud.com>
2021-11-10 15:24:49 +0900

    privateと同じレベルのインデントにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,12 +55,12 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
